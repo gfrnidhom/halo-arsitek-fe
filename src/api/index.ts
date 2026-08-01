e2d7c915b70f90c57f40c676e5a82dbb2009956c
gfrnidhom <gfr.nidhom@gmail.com>
2026-08-02 00:17:06 +0700

    chore: update API baseURL to local network address

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://100.98.214.91:8000',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
